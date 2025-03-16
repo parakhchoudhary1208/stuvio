80c7d528065408837fc197d7a3a966cab9b0d681
parakh2101 <parakhchoudhary1999@gmail.com>
2025-03-16 12:50:34 +0530

    50% task complete

--- src/app/components/SectionRowImg.jsx
+++ src/app/components/SectionRowImg.jsx
@@ -17,25 +17,28 @@ const SectionRowImg = () => {
     const titleRefs = useRef([]);
 
     const handleHover = (index, show) => {
-        clearTimeout(timerRefs.current[index]);
-        if (show) {
-            timerRefs.current[index] = setTimeout(() => {
-                gsap.to(imageRefs.current[index], {
-                    scaleY: 1,
-                    duration: 1,
-                    ease: "none",
-                    transformOrigin: "bottom",
-                });
-            }, 100);
-        } else {
+    clearTimeout(timerRefs.current[index]);
+    if (show) {
+        timerRefs.current[index] = setTimeout(() => {
             gsap.to(imageRefs.current[index], {
-                scaleY: 0,
-                duration: 1,
-                ease: "none",
-                transformOrigin: "top",
+                height: "100%",
+                top: "0%",
+                duration: 0.5,
+                ease: "linear",
             });
-        }
-    };
+        }, 100);
+    } else {
+        gsap.to(imageRefs.current[index], {
+            height: "0%",
+            top: "0%",
+            duration: 0.5,
+            ease: "linear",
+            onComplete: () => {
+                gsap.set(imageRefs.current[index], { top: "100%" });
+            },
+        });
+    }
+};
 
     useGSAP(() => {
         return () => timerRefs.current.forEach(timer => clearTimeout(timer));
@@ -43,8 +46,9 @@ const SectionRowImg = () => {
 
     useEffect(() => {
         titleRefs.current.forEach((titleRef, index) => {
-            gsap.fromTo(titleRef, 
-                { opacity: 0, y: 75 }, 
+            gsap.fromTo(
+                titleRef,
+                { opacity: 0, y: 75 },
                 { opacity: 1, y: 0, duration: 0.5, delay: index * 0.2 }
             );
         });
@@ -55,19 +59,31 @@ const SectionRowImg = () => {
             <section ref={containerRef} className="absolute top-0 left-0 h-full w-full grid lg:grid-cols-4 grid-cols-2">
                 {items.map(({ src, title }, index) => (
                     <div key={index} className="relative h-[50vh] w-[50vw] lg:w-[25vw] lg:h-full border-r border-gray-300 last:border-r-0 grid-item-section">
-                        <div ref={el => imageRefs.current[index] = el} className="absolute h-full w-full overflow-hidden img-wrapper scale-y-0 origin-bottom">
-                            <Image src={src} alt={title} fill style={{ objectFit: "cover" }} className="h-full w-full" />
+                        <div
+                            ref={(el) => (imageRefs.current[index] = el)}
+                            className="absolute h-full w-full overflow-hidden img-wrapper"
+                            style={{ height: "0%", top: "100%" }}
+                        >
+                            <Image
+                                src={src}
+                                alt={title}
+                                fill
+                                style={{ objectFit: "none" }} 
+                                className="h-full w-auto"
+                            />
                         </div>
                         <div className="absolute z-1 bottom-0 left-0 p-6 md:p-4">
-                            <h1 ref={el => titleRefs.current[index * 2] = el} className="font-light text-lg lg:text-4xl uppercase">our</h1>
-                            <h1 ref={el => titleRefs.current[index * 2 + 1] = el} className="font-light text-lg lg:text-4xl uppercase">{title}</h1>
+                            <h1 ref={(el) => (titleRefs.current[index * 2] = el)} className="font-light text-lg lg:text-4xl uppercase">our</h1>
+                            <h1 ref={(el) => (titleRefs.current[index * 2 + 1] = el)} className="font-light text-lg lg:text-4xl uppercase">{title}</h1>
                         </div>
                     </div>
                 ))}
             </section>
             <section className="absolute z-3 bg-transparent top-0 left-0 h-full w-full grid lg:grid-cols-4 grid-cols-2">
                 {items.map((_, index) => (
-                    <div key={index} className="relative h-[50vh] w-[50vw] lg:w-[25vw] lg:h-full" 
+                    <div
+                        key={index}
+                        className="relative h-[50vh] w-[50vw] lg:w-[25vw] lg:h-full"
                         onMouseEnter={() => handleHover(index, true)}
                         onMouseLeave={() => handleHover(index, false)}
                     />
