f5b51556fae922139760f904065d79ea13d39a24
parakh2101 <parakhchoudhary1999@gmail.com>
2025-03-20 14:28:41 +0530

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,6 @@ export default function RootLayout({ children }) {
             <body
                 className={`${poppinsSans.variable} antialiased`} cz-shortcut-listen="true"
             >
-                
                 {children}
             </body>
         </html>
